f821928adb2781099e35d3650198cd78a09da5f2
huijo <ccomkhj@gmail.com>
2023-02-22 12:27:11 +0100

    coco split

--- COCO.py
+++ COCO.py
@@ -1,13 +1,12 @@
 from coco_assistant import COCO_Assistant
-import os
-import glob
-from PIL import Image
-import numpy as np
+from sklearn.model_selection import train_test_split
+from skmultilearn.model_selection import iterative_train_test_split
 import typer
-import yaml
-from tqdm import tqdm
-from rich import print
-from rich.panel import Panel
+import json
+import funcy
+import numpy as np
+from typing import Optional
+from tools.helpers import filter_annotations, filter_images, save_coco
 
 app = typer.Typer(help="Awesome cvOps Tool.", rich_markup_mode="rich")
 
@@ -66,6 +65,96 @@ def merge(
     # Create COCO_Assistant object
     cas = COCO_Assistant(img_dir, ann_dir)
     cas.merge()
+
+
+@app.command()
+def split(
+    ann_path: str = typer.Argument(..., help="Path to COCO annotations file."),
+    train_path: str = typer.Argument(
+        ..., help="Where to store COCO training annotations"
+    ),
+    test_path: str = typer.Argument(..., help="Where to store COCO test annotations"),
+    split: float = typer.Argument(
+        default=0.8, help="A ratio of a split; a number in (0, 1)"
+    ),
+    multi: Optional[bool] = typer.Argument(
+        default=False,
+        help="Split a multi-class dataset while preserving class distributions in train and test sets",
+    ),
+):
+
+    with open(ann_path, "rt", encoding="UTF-8") as annotations:
+        coco = json.load(annotations)
+        info = coco["info"]
+        licenses = coco["licenses"]
+        images = coco["images"]
+        annotations = coco["annotations"]
+        categories = coco["categories"]
+
+        images_with_annotations = funcy.lmap(lambda a: int(a["image_id"]), annotations)
+
+        images = funcy.lremove(lambda i: i["id"] not in images_with_annotations, images)
+
+        if multi:
+
+            annotation_categories = funcy.lmap(
+                lambda a: int(a["category_id"]), annotations
+            )
+
+            # bottle neck 1
+            # remove classes that has only one sample, because it can't be split into the training and testing sets
+            annotation_categories = funcy.lremove(
+                lambda i: annotation_categories.count(i) <= 1, annotation_categories
+            )
+
+            annotations = funcy.lremove(
+                lambda i: i["category_id"] not in annotation_categories, annotations
+            )
+
+            X_train, y_train, X_test, y_test = iterative_train_test_split(
+                np.array([annotations]).T,
+                np.array([annotation_categories]).T,
+                test_size=1 - split,
+            )
+
+            save_coco(
+                train_path,
+                info,
+                licenses,
+                filter_images(images, X_train.reshape(-1)),
+                X_train.reshape(-1).tolist(),
+                categories,
+            )
+            save_coco(
+                test_path,
+                info,
+                licenses,
+                filter_images(images, X_test.reshape(-1)),
+                X_test.reshape(-1).tolist(),
+                categories,
+            )
+
+            print(
+                "Saved {} entries in {} and {} in {}".format(
+                    len(X_train), train_path, len(X_test), test_path
+                )
+            )
+
+        else:
+
+            X_train, X_test = train_test_split(images, train_size=split)
+
+            anns_train = filter_annotations(annotations, X_train)
+            anns_test = filter_annotations(annotations, X_test)
+
+            save_coco(train_path, info, licenses, X_train, anns_train, categories)
+            save_coco(test_path, info, licenses, X_test, anns_test, categories)
+
+            print(
+                "Saved {} entries in {} and {} in {}".format(
+                    len(anns_train), train_path, len(anns_test), test_path
+                )
+            )
 
 
 if __name__ == "__main__":
--- tools/helpers.py
+++ tools/helpers.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 from PIL import Image
 from typing import Dict, List
+import json
+import funcy
 
 
 def load(path: str) -> List[str]:
@@ -37,3 +39,31 @@ def palette2mask(ann: str, conv: Dict[int, List]) -> np.ndarray:
         drawing[region] = key
 
     return drawing
+
+
+def save_coco(file, info, licenses, images, annotations, categories):
+    with open(file, "wt", encoding="UTF-8") as coco:
+        json.dump(
+            {
+                "info": info,
+                "licenses": licenses,
+                "images": images,
+                "annotations": annotations,
+                "categories": categories,
+            },
+            coco,
+            indent=2,
+            sort_keys=True,
+        )
+
+
+def filter_annotations(annotations, images):
+    image_ids = funcy.lmap(lambda i: int(i["id"]), images)
+    return funcy.lfilter(lambda a: int(a["image_id"]) in image_ids, annotations)
+
+
+def filter_images(images, annotations):
+
+    annotation_ids = funcy.lmap(lambda i: int(i["image_id"]), annotations)
+
+    return funcy.lfilter(lambda a: int(a["id"]) in annotation_ids, images)
