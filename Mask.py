16c7438bf6d380ec860e1d1709111a7496b32619
huijo <ccomkhj@gmail.com>
2023-04-05 12:02:28 +0200

    show the unique RGB value for config

--- Mask.py
+++ Mask.py
@@ -2,6 +2,7 @@ import os
 
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 import typer
 import yaml
 from PIL import Image
@@ -80,9 +81,25 @@ def show(
     for ann in anns:
         mask = cv2.cvtColor(cv2.imread(ann), cv2.COLOR_BGR2GRAY)
         plt.imshow(mask, cmap="Paired")
-        plt.title(f"overlap image of {os.path.basename(ann)}")
+        plt.title(f"Mask of {os.path.basename(ann)}")
         plt.show()
 
+@app.command()
+def checkcolor(
+    mask_dir: str = typer.Argument(..., help="directory of binary(-like) masks."),
+):
+    """
+    [bold green]Visualize mask sample[/bold green]
+    """
+    anns = load(mask_dir)
+
+    for ann in anns:
+
+        mask = cv2.cvtColor(cv2.imread(ann), cv2.COLOR_BGR2RGB)
+        plt.imshow(mask)
+        print(f"Mask of {os.path.basename(ann)}. Palletes are: {np.unique(mask.reshape(-1, mask.shape[2]), axis=0)}")
+        plt.title(f"Mask of {os.path.basename(ann)}. Palletes are: {np.unique(mask.reshape(-1, mask.shape[2]), axis=0)}")
+        plt.show()
 
 if __name__ == "__main__":
     app()
