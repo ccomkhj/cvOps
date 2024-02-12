7e5062be27b6f5aa51853f0861193ccf050a4fea
ccomkhj <ccomkhj@gmail.com>
2024-02-12 13:45:51 +0100

    visualizebox works handling deprecation

--- COCO.py
+++ COCO.py
@@ -9,15 +9,30 @@ import time
 import yaml
 from tqdm import tqdm
 from coco_assistant import COCO_Assistant
+from coco_assistant import coco_visualiser as cocovis
 from PIL import Image
 from pycocotools.coco import COCO
 from sklearn.model_selection import train_test_split
 from skmultilearn.model_selection import iterative_train_test_split
 
-from tools.helpers import filter_annotations, filter_images, locate_images, save_coco, get_image_files
+from tools.helpers import (
+    filter_annotations,
+    filter_images,
+    locate_images,
+    save_coco,
+    get_image_files,
+)
 
 import tkinter as tk
-from tools.cocoviewer import Data, StatusBar, SlidersBar, ObjectsPanel, Menu, ImagePanel, Controller
+from tools.cocoviewer import (
+    Data,
+    StatusBar,
+    SlidersBar,
+    ObjectsPanel,
+    Menu,
+    ImagePanel,
+    Controller,
+)
 
 app = typer.Typer(help="Awesome cvOps Tool.", rich_markup_mode="rich")
 
@@ -25,7 +40,7 @@ app = typer.Typer(help="Awesome cvOps Tool.", rich_markup_mode="rich")
 @app.command()
 def visualize(
     img_dir: str = typer.Argument(..., help="directory of images"),
-    ann_dir: str = typer.Argument(..., help="directory of annotations"),
+    ann: str = typer.Argument(..., help="path of annotations"),
 ):
     """
     [bold green]Visualize coco sample[/bold green]
@@ -34,20 +49,17 @@ def visualize(
     Example:
     .
     ├── images_dir
-    │   ├── sample1
-    │   ├── sample2
-    |   ├── sample3
+    │   ├── image1
+    │   ├── image2
+    |   ├── image3
     |
-    ├── anns_dir
-    │   ├── sample1.json
-    │   ├── sample2.json
-    │   ├── sample3.json
+    ├── ann_path
     [/blue]
     """
 
     # Create COCO_Assistant object
-    cas = COCO_Assistant(img_dir, ann_dir)
-    cas.visualise()
+    cocovis.visualise_all(COCO(ann), img_dir)
+
 
 @app.command()
 def visualizebox(
@@ -91,7 +103,7 @@ def merge(
     merge_images: bool = typer.Option(
         False,
         help="Merge also images with the flag of --merge_images",
-    ) 
+    ),
 ):
     """
     [bold green]Merge coco sample[/bold green]
@@ -153,8 +165,6 @@ def convert(
         )
         Image.fromarray(mask).convert("P").save(file_path)
         print(f"Successfully generated mask file: {imgFileName}")
-
-
 
 
 @app.command()
@@ -255,7 +265,8 @@ def split(
         parent_dir = os.path.dirname(train_path)
         grand_parent = os.path.dirname(os.path.dirname(parent_dir))
         destDir_train = os.path.join(
-            grand_parent, "train", "images", "new_train_images")
+            grand_parent, "train", "images", "new_train_images"
+        )
         os.makedirs(destDir_train, exist_ok=True)
 
         # Iterate the dictionary and send it to source and destination.
@@ -266,10 +277,9 @@ def split(
             locate_images(source, destination)
 
         # if the path is given,
-        destDir_test = os.path.join(
-            grand_parent, "val", "images", "new_val_images")
+        destDir_test = os.path.join(grand_parent, "val", "images", "new_val_images")
         os.makedirs(destDir_test, exist_ok=True)
-        
+
         for test_obj in X_test:
             file_name = test_obj.get("file_name")
             source = os.path.join(image_locate, file_name)
@@ -280,9 +290,12 @@ def split(
 @app.command()
 def update(
     new_ann_path: str = typer.Argument(..., help="Path to new COCO annotations file."),
-    train_ann_path: str = typer.Argument(..., help="Path to existing train COCO annotations file."),
-    val_ann_path: str = typer.Argument(...,
-                                       help="Path to existing val COCO annotations file."),
+    train_ann_path: str = typer.Argument(
+        ..., help="Path to existing train COCO annotations file."
+    ),
+    val_ann_path: str = typer.Argument(
+        ..., help="Path to existing val COCO annotations file."
+    ),
     split_ratio: float = typer.Argument(
         default=0.8, help="A ratio of a split; a number in (0, 1)"
     ),
@@ -290,58 +303,67 @@ def update(
         default="", help="Locate images based on the split if the value is given."
     ),
 ):
-    assert validate(image_locate, new_ann_path) is True, "Images in coco and image DIR doesn't match."
-    
+    assert (
+        validate(image_locate, new_ann_path) is True
+    ), "Images in coco and image DIR doesn't match."
+
     now = int(time.time())
     outcome_path = f"results/{now}"
-    
+
     # Setup paths
     outcome_train_ann = os.path.join(outcome_path, "train", "ann")
     outcome_val_ann = os.path.join(outcome_path, "val", "ann")
     outcome_train_img = os.path.join(outcome_path, "train", "images")
     outcome_val_img = os.path.join(outcome_path, "val", "images")
-    
+
     # Create relevant folders
     os.makedirs(outcome_train_ann, exist_ok=True)
     os.makedirs(outcome_val_ann, exist_ok=True)
     os.makedirs(os.path.join(outcome_train_img, "existing_train"), exist_ok=True)
     os.makedirs(os.path.join(outcome_val_img, "existing_val"), exist_ok=True)
-    
-    split(new_ann_path, os.path.join(outcome_train_ann, "new_train_images.json"),
-          os.path.join(outcome_val_ann, "new_val_images.json"), split_ratio, image_locate, multi=False)
+
+    split(
+        new_ann_path,
+        os.path.join(outcome_train_ann, "new_train_images.json"),
+        os.path.join(outcome_val_ann, "new_val_images.json"),
+        split_ratio,
+        image_locate,
+        multi=False,
+    )
 
     # Move splitted images
-    locate_images(train_ann_path, os.path.join(
-        outcome_train_ann, "existing_train.json"))
+    locate_images(
+        train_ann_path, os.path.join(outcome_train_ann, "existing_train.json")
+    )
     locate_images(val_ann_path, os.path.join(outcome_val_ann, "existing_val.json"))
-    
-    # Merge train and val coco json
-    merge(outcome_train_img, outcome_train_ann, merge_images= False)
-    merge(outcome_val_img,
-          outcome_val_ann, merge_images=False)
 
+    # Merge train and val coco json
+    merge(outcome_train_img, outcome_train_ann, merge_images=False)
+    merge(outcome_val_img, outcome_val_ann, merge_images=False)
 
 
 @app.command()
 def validate(
     img_path: str = typer.Argument(..., help="Path to image files"),
-    ann_path: str = typer.Argument(..., help="Path to COCO annotations file")
+    ann_path: str = typer.Argument(..., help="Path to COCO annotations file"),
 ):
-    '''
+    """
     This code validates if all images in coco exist in img_path.
-    '''
-    
+    """
+
     filenames_in_folder = get_image_files(img_path)
     filenames_in_folder = [os.path.basename(i) for i in filenames_in_folder]
-    
+
     coco = COCO(ann_path)
-    
+
     img_ids = coco.getImgIds()
-    
+
     # Get filenames for each image
-    filenames_in_coco = [os.path.basename(coco.loadImgs(image_id)[0]['file_name'])
-                 for image_id in img_ids]
-    
+    filenames_in_coco = [
+        os.path.basename(coco.loadImgs(image_id)[0]["file_name"])
+        for image_id in img_ids
+    ]
+
     if sorted(filenames_in_folder) == sorted(filenames_in_coco):
         print("Filenames in folder and filenames in coco exactly matches!!")
         return True
@@ -362,20 +384,21 @@ def validate(
         print("This files are not matching!! Please double check!!", result)
         return False
 
+
 @app.command()
 def delete(
     config: str = typer.Argument(..., help="Path to category manage config file"),
-    ann_path: str = typer.Argument(..., help="Path to COCO annotations file")
+    ann_path: str = typer.Argument(..., help="Path to COCO annotations file"),
 ):
-    '''
+    """
     This code referes to COCO-Assistant > remove_cat.
-    '''
+    """
 
-    with open(config, 'r') as file:
+    with open(config, "r") as file:
         config_catman = yaml.safe_load(file)
-    
+
     # list of cagetories to be removed
-    rcats = config_catman['delete']
+    rcats = config_catman["delete"]
 
     coco = COCO(ann_path)
 
@@ -383,7 +406,7 @@ def delete(
     catids_remove = coco.getCatIds(catNms=rcats)
 
     if len(catids_remove) == 0:
-        print('Nothing to be removed.')
+        print("Nothing to be removed.")
         return
 
     # Gives you a list of ids of annotations that contain those categories
@@ -404,30 +427,30 @@ def delete(
 
     directory = os.path.dirname(ann_path)
     filename = os.path.splitext(os.path.basename(ann_path))[0]
-    with open(os.path.join(directory, filename + '_delete.json'), "w") as oa:
+    with open(os.path.join(directory, filename + "_delete.json"), "w") as oa:
         json.dump(ann, oa, indent=4)
 
-    print('Successfully deleted the desired categories.')
+    print("Successfully deleted the desired categories.")
 
 
 @app.command()
 def process(
     config: str = typer.Argument(..., help="Path to category manage config file"),
-    ann_path: str = typer.Argument(..., help="Path to COCO annotations file")
+    ann_path: str = typer.Argument(..., help="Path to COCO annotations file"),
 ):
-    with open(config, 'r') as file:
+    with open(config, "r") as file:
         config_catman = yaml.safe_load(file)
-    
-    # list of cagetories to be processed
-    pcat = config_catman['process']
 
-    with open(ann_path, 'r') as file:
+    # list of cagetories to be processed
+    pcat = config_catman["process"]
+
+    with open(ann_path, "r") as file:
         ann = json.load(file)
-    
+
     unique_categories = {}
     convert_category_id = {}
 
-    for category in ann['categories']:
+    for category in ann["categories"]:
         if category["name"] in pcat:
             category["name"] = pcat[category["name"]]
 
@@ -435,7 +458,7 @@ def process(
             if category["name"] in unique_categories:
                 # Category name already exists, update relevant attribute
                 existing_category = unique_categories[category["name"]]
-                existing_category_id = existing_category['id']
+                existing_category_id = existing_category["id"]
 
                 convert_category_id[category["id"]] = existing_category_id
             else:
@@ -446,42 +469,38 @@ def process(
                 unique_categories[category["name"]] = category
 
     # Replace the categories in the Coco data
-    ann['categories'] = list(unique_categories.values())
+    ann["categories"] = list(unique_categories.values())
 
     for annotation in ann["annotations"]:
         annotation["category_id"] = convert_category_id[annotation["category_id"]]
 
     directory = os.path.dirname(ann_path)
     filename = os.path.splitext(os.path.basename(ann_path))[0]
-    with open(os.path.join(directory, filename + '_process.json'), "w") as oa:
+    with open(os.path.join(directory, filename + "_process.json"), "w") as oa:
         json.dump(ann, oa, indent=4)
 
-    print('Successfully processed the categories and annotations.')
+    print("Successfully processed the categories and annotations.")
 
 
 @app.command()
 def convertjsonformat(
     json_aihub_dir: str = typer.Argument(..., help="directory of AI Hub json files"),
-    json_coco_dir: str = typer.Argument(..., help="directory to save converted COCO json file"),
+    json_coco_dir: str = typer.Argument(
+        ..., help="directory to save converted COCO json file"
+    ),
 ):
     json_files = [file for file in os.listdir(json_aihub_dir) if file.endswith(".json")]
     json_files.sort()
 
     coco = {
-                "info": {},
-                "licenses": [
-                    {
-                        "id": 0,
-                        "name": "",
-                        "url": ""
-                    }
-                ],
-                "categories": [],
-                "images": [],
-                "annotations": []
-            }
+        "info": {},
+        "licenses": [{"id": 0, "name": "", "url": ""}],
+        "categories": [],
+        "images": [],
+        "annotations": [],
+    }
 
-    for aihub_file in tqdm(json_files, desc='Processing', unit='item'):
+    for aihub_file in tqdm(json_files, desc="Processing", unit="item"):
         if os.path.isfile(os.path.join(json_aihub_dir, aihub_file)):
             # Load the JSON file
             with open(os.path.join(json_aihub_dir, aihub_file), "r") as file:
@@ -494,10 +513,7 @@ def convertjsonformat(
                 coco["info"]["flags"] = aihub["flags"]
 
             # Add Image data
-            image_data = {
-                "id": len(coco["images"]),
-                "license": 0
-            }
+            image_data = {"id": len(coco["images"]), "license": 0}
             if "imagePath" in aihub:
                 image_data["file_name"] = aihub["imagePath"]
             if "imageHeight" in aihub:
@@ -524,15 +540,17 @@ def convertjsonformat(
                         if category["name"] == shape_label:
                             category_id = category["id"]
                             break
-                    
+
                     # If the shape label is not present in categories, add it
                     if category_id is None:
                         category_id = len(coco["categories"])
-                        coco["categories"].append({
-                            "id": category_id,
-                            "name": shape_label,
-                            "supercategory": shape["group_id"]
-                        })
+                        coco["categories"].append(
+                            {
+                                "id": category_id,
+                                "name": shape_label,
+                                "supercategory": shape["group_id"],
+                            }
+                        )
 
                     # Add annotation
                     points = shape["points"]
@@ -543,21 +561,30 @@ def convertjsonformat(
                         "bbox": [],
                         "area": 0,
                         "segmentation": [],
-                        "iscrowd": 0
+                        "iscrowd": 0,
                     }
                     if shape["shape_type"] == "rectangle":
-                        annotation["bbox"] = [points[0][0], \
-                                                points[0][1], \
-                                                points[1][0] - points[0][0], \
-                                                points[1][1] - points[0][1]]
-                        annotation["area"] = (points[1][0] - points[0][0]) * \
-                                             (points[1][1] - points[0][1])
-                        annotation["segmentation"].append([
-                            points[0][0], points[0][1],
-                            points[1][0], points[0][1],
-                            points[1][0], points[1][1],
-                            points[0][0], points[1][1]
-                        ])
+                        annotation["bbox"] = [
+                            points[0][0],
+                            points[0][1],
+                            points[1][0] - points[0][0],
+                            points[1][1] - points[0][1],
+                        ]
+                        annotation["area"] = (points[1][0] - points[0][0]) * (
+                            points[1][1] - points[0][1]
+                        )
+                        annotation["segmentation"].append(
+                            [
+                                points[0][0],
+                                points[0][1],
+                                points[1][0],
+                                points[0][1],
+                                points[1][0],
+                                points[1][1],
+                                points[0][0],
+                                points[1][1],
+                            ]
+                        )
                     elif shape["shape_type"] == "polygon":
                         minX = maxX = points[0][0]
                         minY = maxY = points[0][1]
@@ -578,15 +605,17 @@ def convertjsonformat(
                         for i in range(len(points)):
                             x1, y1 = points[i]
                             x2, y2 = points[(i + 1) % len(points)]
-                            area += (x1 * y2 - x2 * y1)
+                            area += x1 * y2 - x2 * y1
                         annotation["area"] = abs(area) * 0.5
-                        
-                    coco["annotations"].append(annotation)    
+
+                    coco["annotations"].append(annotation)
         else:
             print(f"{os.path.join(json_aihub_dir, aihub_file)} does not exist.")
 
     # Save the Python object as JSON
-    with open(os.path.join(json_coco_dir, f"{os.path.basename(json_aihub_dir)}.json"), "w") as file:
+    with open(
+        os.path.join(json_coco_dir, f"{os.path.basename(json_aihub_dir)}.json"), "w"
+    ) as file:
         json.dump(coco, file, indent=4)
 
     print("Successfully converted AiHub json format to COCO json format")
@@ -595,22 +624,23 @@ def convertjsonformat(
 @app.command()
 def replaceimgformat(
     annotation: str = typer.Argument(..., help="COCO json annotation file"),
-    format: str = typer.Argument(..., help="Desired img format you want to replace")
+    format: str = typer.Argument(..., help="Desired img format you want to replace"),
 ):
     if os.path.isfile(annotation):
         with open(annotation, "r") as file:
             ann = json.load(file)
-        
+
         # Replace the image extension to desired format
         for ann_img in ann["images"]:
             ann_img["file_name"] = ann_img["file_name"].split(".")[0] + "." + format
 
-         # Save the Python object as JSON
+        # Save the Python object as JSON
         with open(annotation, "w") as file:
             json.dump(ann, file, indent=4)
-    
+
     else:
         print(f"{annotation} does not exist.")
+
 
 if __name__ == "__main__":
     app()
