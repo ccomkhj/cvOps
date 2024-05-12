3ad1fb049f8a9629c617d01ab957c7129f294031
ccomkhj <ccomkhj@gmail.com>
2024-05-12 12:46:19 +0200

    introduce icon

--- cvops/main_window.py
+++ cvops/main_window.py
@@ -7,7 +7,8 @@ from PyQt5.QtWidgets import (
     QWidget,
     QFrame,
 )
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QPalette, QColor, QIcon, QFont
 
 # Assuming dialogs are in a package named "dialogs"
 from dialogs import (
@@ -30,25 +31,35 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("COCO Tools")
         self.setGeometry(100, 100, 800, 600)
 
+        font = QFont("Arial", 12)
+        icon_size = QSize(40, 40)
+
         self.centralWidget = QWidget(self)
         self.setCentralWidget(self.centralWidget)
 
         gridLayout = QGridLayout(self.centralWidget)
 
         # Button size
-        buttonSize = QSize(150, 80)
-        buttonSizeBig = QSize(150, 160)
+        buttonSize = QSize(200, 80)
+        buttonSizeBig = QSize(200, 160)
 
-        # Visualize and Remap Categories Buttons
+        # Visualize Button (previously configured)
         self.visualizeButton = QPushButton("Visualize", self.centralWidget)
         self.visualizeButton.setFixedSize(buttonSize)
+        self.visualizeButton.setIcon(QIcon("data/icons/visualization.png"))
+        self.visualizeButton.setIconSize(icon_size)
+        self.visualizeButton.setFont(font)
         self.visualizeButton.setToolTip("Visualize the annotations within the dataset.")
         self.visualizeButton.clicked.connect(self.showVisualizeDialog)
+
+        # Remap Categories Button
         self.remapCategoriesButton = QPushButton("Remap Categories", self.centralWidget)
         self.remapCategoriesButton.setFixedSize(buttonSize)
+        self.remapCategoriesButton.setIcon(QIcon("data/icons/remap.png"))
+        self.remapCategoriesButton.setIconSize(icon_size)
+        self.remapCategoriesButton.setFont(font)
         self.remapCategoriesButton.setToolTip("Remap the category ids.")
         self.remapCategoriesButton.clicked.connect(self.showRemapCategoriesDialog)
-
         gridLayout.addWidget(self.visualizeButton, 0, 0)
         gridLayout.addWidget(self.remapCategoriesButton, 0, 1)
 
@@ -58,13 +69,21 @@ class MainWindow(QMainWindow):
         divider1.setFrameShadow(QFrame.Sunken)
         gridLayout.addWidget(divider1, 1, 0, 1, -1)  # Span all columns
 
-        # Split and Merge Buttons
+        # Split Button
         self.splitButton = QPushButton("Split", self.centralWidget)
         self.splitButton.setFixedSize(buttonSize)
+        self.splitButton.setIcon(QIcon("data/icons/split.png"))
+        self.splitButton.setIconSize(icon_size)
+        self.splitButton.setFont(font)
         self.splitButton.setToolTip("Split COCO into train and test.")
         self.splitButton.clicked.connect(self.showSplitDialog)
+
+        # Merge Button
         self.mergeButton = QPushButton("Merge", self.centralWidget)
         self.mergeButton.setFixedSize(buttonSize)
+        self.mergeButton.setIcon(QIcon("data/icons/merge.png"))
+        self.mergeButton.setIconSize(icon_size)
+        self.mergeButton.setFont(font)
         self.mergeButton.setToolTip("Merge COCO files.")
         self.mergeButton.clicked.connect(self.showMergeDialog)
 
@@ -77,27 +96,38 @@ class MainWindow(QMainWindow):
         divider2.setFrameShadow(QFrame.Sunken)
         gridLayout.addWidget(divider2, 3, 0, 1, -1)  # Span all columns
 
-        # Update, Post Update (spanning two rows), and S3 Update Buttons
+        # Update (Local) Button
         self.updateButton = QPushButton("Update (Local)", self.centralWidget)
         self.updateButton.setFixedSize(buttonSize)
+        self.updateButton.setIcon(QIcon("data/icons/update_local.png"))
+        self.updateButton.setIconSize(icon_size)
+        self.updateButton.setFont(font)
         self.updateButton.setToolTip(
-            "Update the existing COCO dataset with new COCO dataset. File is in the local machine."
+            "Update the existing COCO dataset with new COCO dataset on local machine."
         )
         self.updateButton.clicked.connect(self.showUpdateDialog)
 
+        # Post Update Button (Noting it spans two rows)
         self.postUpdateButton = QPushButton("Post Update", self.centralWidget)
-        self.postUpdateButton.setFixedSize(buttonSizeBig)
+        self.postUpdateButton.setFixedSize(
+            buttonSizeBig
+        )  # As previously specified to be bigger
+        self.postUpdateButton.setIcon(QIcon("data/icons/post_update.png"))
+        self.postUpdateButton.setIconSize(icon_size)
+        self.postUpdateButton.setFont(font)
         self.postUpdateButton.setToolTip(
-            """After Update process, decide if you want to combine. 
-It consists visualization of train and validation set. 
-Afterwards, you can directly upload to S3 bucket."""
+            "After Update process, decide if you want to combine. It consists visualization of train and validation set. Afterwards, you can directly upload to S3 bucket."
         )
         self.postUpdateButton.clicked.connect(self.showPostUpdateDialog)
 
+        # Update (S3) Button
         self.s3UpdateButton = QPushButton("Update (S3)", self.centralWidget)
         self.s3UpdateButton.setFixedSize(buttonSize)
+        self.s3UpdateButton.setIcon(QIcon("data/icons/update_s3.png"))
+        self.s3UpdateButton.setIconSize(icon_size)
+        self.s3UpdateButton.setFont(font)
         self.s3UpdateButton.setToolTip(
-            "Update the existing COCO dataset with new COCO dataset. File is in AWS S3. Configure under `config/s3_credentials.yaml`"
+            "Update the existing COCO dataset with new COCO dataset. File is in AWS S3."
         )
         self.s3UpdateButton.clicked.connect(self.showS3UpdateDialog)
 
@@ -136,6 +166,15 @@ Afterwards, you can directly upload to S3 bucket."""
 
 def main():
     app = QApplication(sys.argv)
+    # Force the style to be the same on all OSs:
+    app.setStyle("Fusion")
+
+    # Now use a palette to switch to dark colors:
+    palette = QPalette()
+    palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.WindowText, Qt.white)
+
+    app.setPalette(palette)
     mainWindow = MainWindow()
     mainWindow.show()
     sys.exit(app.exec_())
